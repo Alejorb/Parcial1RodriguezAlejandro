4cbce08e6c909aea60eaa1e657a4ca896930e72b
alejorb <luis.rodriguezbenalcazar@pucese.edu.ec>
2019-12-04 11:52:15 -0500

    Agregar archivos de proyecto.

---
+++ Parcial1RodriguezAlejandro/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Parcial1RodriguezAlejandro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEntero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrimo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.brnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEntero
+            // 
+            this.txtEntero.Location = new System.Drawing.Point(216, 83);
+            this.txtEntero.Name = "txtEntero";
+            this.txtEntero.Size = new System.Drawing.Size(159, 20);
+            this.txtEntero.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N° de Primos";
+            // 
+            // txtPrimo
+            // 
+            this.txtPrimo.Location = new System.Drawing.Point(216, 165);
+            this.txtPrimo.Name = "txtPrimo";
+            this.txtPrimo.Size = new System.Drawing.Size(159, 20);
+            this.txtPrimo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Suma de n° Primos";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(455, 165);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(106, 45);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // brnCalcular
+            // 
+            this.brnCalcular.Location = new System.Drawing.Point(455, 67);
+            this.brnCalcular.Name = "brnCalcular";
+            this.brnCalcular.Size = new System.Drawing.Size(106, 45);
+            this.brnCalcular.TabIndex = 6;
+            this.brnCalcular.Text = "Calcular";
+            this.brnCalcular.UseVisualStyleBackColor = true;
+            this.brnCalcular.Click += new System.EventHandler(this.brnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.brnCalcular);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrimo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEntero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtEntero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrimo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button brnCalcular;
+    }
+}
+
---
+++ Parcial1RodriguezAlejandro/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1RodriguezAlejandro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void brnCalcular_Click(object sender, EventArgs e)
+        {
+            int limite = 0, suma = 0;
+            limite = int.Parse(this.txtEntero.Text);
+            suma = Clases.Examen.sumatoria(limite);
+            this.txtPrimo.Text = suma.ToString();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+
